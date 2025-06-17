2acac923f060e46ce597991b29eeb4ad7ee90467
temcray <ejamison06@gmail.com>
2025-06-16 21:42:33 -0600

    variables

---
+++ scripts.js
@@ -0,0 +1,50 @@
+console.log("script.js")
+//variables
+
+//Strings
+let firstName = "Tatiana";
+let lastName = "McCray";
+
+
+//numerial
+let age = 99;
+let mySize = 10;
+
+//boolean values
+let isStudent = false;
+
+console.log("firstname");
+console.log("age,mySize");
+
+//concatenation
+console.log("Tatiana" + firstName + "McCray" + lastName);
+
+//Operation
+
+let num1 = 10;
+let num = 2;
+
+let addition = num1 + num2;
+let sub = num1 - num2;
+let div = num1 / num2;
+let mult = num1 * num2;
+//sub,div, mult
+
+console.log("Add is" + add);
+console.log('Sub is:' + subs);
+console.log("Div is:" + div);
+console.log("Mult is" + mult);
+
+let aVer = 20;
+let otherVar = 30;
+
+console.log(aVar,otherVar);
+
+aVar = 50;
+otherVar = 60;
+
+console.log( aVer,otherVar);
+
+const weekDays = 5;
+
+console.log(weekDays);
