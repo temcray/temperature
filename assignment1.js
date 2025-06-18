ce6cf80f6f8f9cb67767522eb0df950772cbfea4
temcray <ejamison06@gmail.com>
2025-06-18 17:22:22 -0600

    assignment1

--- assignment1.js
+++ assignment1.js
@@ -1,28 +1,29 @@
 //1. Declare Variabes
 // Mishelle's day out
 let character = "Michelle the cat";
-let action = "Going outside";
+let action = "sleeping";
 let object = "Hungary";
-let place = "Home";
-let time = "a cold dark night";
+let place = "by window";
+let time = " night";
 
 //using "const"
 const occasion = "a front door is open";
-
+ 
 //2.Crate the Story
 console.log("-----Michelle's day out------");
-console.log(character +" was at " + place +" when " + action + " sleeping");
-console.log("Then her owner came in the front door" + character + "from night groceries shopping" + place + "she smell food" + time + "the door close");
+console.log(character + " was at " + place + " home " + action + " her owner open the front door.");
+console.log("Then her owner came in " + character + " wake u " + place + " she smell food " + time + " the door close ");
 console.log("It's cold out out here, I'm go around back" + occasion + "owner call out Michelle where are you? She from around to the front and they are happy!");
 
 //3. Experimennt with Different Values
 
-character= "Kelly";
-action= "Running";
+character = "Kelly";
+action = "Running";
 object = "exercising";
 place = "The park";
 time = "sunny day";
 
-console.log(character + " was in " +  place +" enjoying " + time + " before she start her run.");
-console.log("She dose her stretches " + character + " begins to " + object + " by running laps around the park " + action + " she is going hard today.");
-console.log("She is determind to get her body back" + occasion + " but gets back up and run her final lap, she is happy with herself.");
+console.log(character + " was in " + place + " enjoying " + time + " before she start her run.");
+console.log("She dose her stretches " + character + " begins to " + object + " by running laps around the park. " + action + " she is going hard today.");
+console.log("She is determind to get her body back" + occasion + " final lap, she is happy with herself.");
+ 
