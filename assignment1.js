1a31a75b85fd522565e0f09fe8adaa54a354592d
temcray <ejamison06@gmail.com>
2025-06-18 17:02:21 -0600

    assignment 1

---
+++ assignment1.js
@@ -0,0 +1,28 @@
+//1. Declare Variabes
+// Mishelle's day out
+let character = "Michelle the cat";
+let action = "Going outside";
+let object = "Hungary";
+let place = "Home";
+let time = "a cold dark night";
+
+//using "const"
+const occasion = "a front door is open";
+
+//2.Crate the Story
+console.log("-----Michelle's day out------");
+console.log(character +" was at " + place +" when " + action + " sleeping");
+console.log("Then her owner came in the front door" + character + "from night groceries shopping" + place + "she smell food" + time + "the door close");
+console.log("It's cold out out here, I'm go around back" + occasion + "owner call out Michelle where are you? She from around to the front and they are happy!");
+
+//3. Experimennt with Different Values
+
+character= "Kelly";
+action= "Running";
+object = "exercising";
+place = "The park";
+time = "sunny day";
+
+console.log(character + " was in " +  place +" enjoying " + time + " before she start her run.");
+console.log("She dose her stretches " + character + " begins to " + object + " by running laps around the park " + action + " she is going hard today.");
+console.log("She is determind to get her body back" + occasion + " but gets back up and run her final lap, she is happy with herself.");
