cc01064db007b2411246adfce743d5d954f5804d
temcray <ejamison06@gmail.com>
2025-06-19 22:41:40 -0600

    assignment2

---
+++ assignment2.html/assignment2.js
@@ -0,0 +1,12 @@
+console.log("temperature");
+function converttofahrenheit(){
+    let ceisius=prompt("9/5");
+    console.log("Convert to Fahrenheit");
+}
+
+converttofahrenheit();
+converttofahrenheit();
+
+function Fahrenheit(){
+    Fahrenheit ("celsius * 9/5");
+}
